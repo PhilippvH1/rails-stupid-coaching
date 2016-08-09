e1426c23794567b646923d8f041ce580fb069f1e
Philipp von Hammerstein <p.vonhammerstein@whu.edu>
2016-08-09 11:37:21 +0200

    <3 Lars

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,33 @@
+class PagesController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @answer = coach_answer_enhanced(params[:query])
+  end
+
+  private
+
+
+  def coach_answer(your_message)
+    if your_message.upcase.include? "I AM GOING TO WORK RIGHT NOW!"
+      return ""
+    elsif your_message.include? "?"
+      return "Silly question, get dressed and go to work!"
+    else
+      return "I don't care, get dressed and go to work!"
+    end
+  end
+
+  def coach_answer_enhanced(your_message)
+    if your_message == "I AM GOING TO WORK RIGHT NOW!"
+      return ""
+    elsif your_message == your_message.upcase
+      return "I can feel your motivation! #{coach_answer(your_message)}"
+    else
+      coach_answer(your_message)
+    end
+  end
+end
+
+
